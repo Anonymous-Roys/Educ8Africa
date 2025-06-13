3ae5186884ef108bd76049975acd829c90f4f04d
Frank Gbadago <slymawuli3@gmail.com>
2025-06-13 22:14:40 +0000

    feat : add Nss page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
 import ErrorPage from './pages/ErrorPage';
 import './App.css'
+import Nss from './pages/nss';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<Home/>} />
+        <Route path = "/nss" element ={<Nss/>}/>
         {/* Add more routes for other components */}
         <Route path="*" element={<ErrorPage/>} /> {/* Catch-all route for errors */}
       </Routes>
---
+++ src/pages/nss.jsx
@@ -0,0 +1,10 @@
+
+export default function  Nss() {
+  return (
+    <div className ="text-lg  flex-1 bg-red-500 h-screen ">
+        <h1 className="text-3xl font-bold text-center text-white pt-20">NSS Page</h1>
+        <p className="text-center text-white mt-4">This is the NSS page content.</p>
+        
+    </div>
+  )
+}
